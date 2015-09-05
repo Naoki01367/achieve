842f2844ab517fb6827aa5bb3bd640f0ec66019f
Naoki Sugiyama <naokisfc32@gmail.com>
2015-09-05 09:12:47 +0000

    day5

--- app/controllers/inquiry_controller.rb
+++ app/controllers/inquiry_controller.rb
@@ -1,4 +1,5 @@
 class InquiryController < ApplicationController
+  before_filter :authenticate_user!
   def index
     @inquiry = Inquiry.new
     render :action => 'index'
@@ -15,7 +16,7 @@ class InquiryController < ApplicationController
 
   def thanks
     @inquiry = Inquiry.new(inquiry_params)
-    # send mail
+    @inquiry.save
     InquiryMailer.received_email(@inquiry).deliver
 
     flash[:notice] = "お問い合わせ頂き、ありがとうございました。"
